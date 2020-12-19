caf662a3bffb9470573e97378d2e4e57ea0c1d27
sigdelbjay <sigdelbjay@gmail.com>
2020-12-19 13:39:24 +1100

    adds retrofit

---
+++ app/src/main/java/com/android/newsapp/api/NewsAPI.kt
@@ -0,0 +1,29 @@
+package com.android.newsapp.api
+
+import com.android.newsapp.NewsResponse
+import com.android.newsapp.util.Constants.Companion.API_KEY
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface NewsAPI {
+    @GET("v2/top-headlines")
+    suspend fun getBreakingNews(
+            @Query("country")
+            countryCode: String = "us",
+            @Query("page")
+            pageNumber: Int = 1,
+            @Query("API_KEY")
+            apiKey: String = API_KEY
+    ): Response<NewsResponse>
+
+    @GET("v2/everything")
+    suspend fun getSearchNews(
+            @Query("q")
+            searchQuery: String,
+            @Query("page")
+            pageNumber: Int = 1,
+            @Query("API_KEY")
+            apiKey: String = API_KEY
+    ): Response<NewsResponse>
+}
---
+++ app/src/main/java/com/android/newsapp/util/Constants.kt
@@ -0,0 +1,8 @@
+package com.android.newsapp.util
+
+class Constants {
+    companion object {
+        const val API_KEY = "b76de9aef27b4886967f363b9f501e40"
+        const val BASE_URL = "http://newsapi.org/"
+    }
+}
